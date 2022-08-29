c1f0dc1def9af6e3fd10b262b9caf69272c4d34e
alim <7475149@mail.ru>
2022-08-29 23:07:47 +0600

    решена вторая (21) задача

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
 ﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
-bool Razvorot(string dannye)                                      // метод сравнения чисел
+/*
+bool Razvorot(string dannye)                                      // метод сравнения чисел сначала думал разворачивать число, но передумал так как возникли нестыковки с типами данных и излишние конвертации
 {
+    int count = 0;
     int len = dannye.Length;                                      // находим длину
     for (int i = 1, j = len - 1; i < j; i++, j--)                 // запускаем цикл при котором есть 2 переменные i и j  для сравнения слов одно сначала другое с конца
-        if (dannye[i] != dannye[j]) return false;                 // если не равно то false 
-        return true;                                              // если равно то true 
+    {
+        if (dannye[i] != dannye[j])                               // если не соответствует 
+        { 
+            count ++;                                             // то увеличиваем count на 1 это означает что есть асимметрия (счетчик асимметрии)
+        }
+    }
+    if (count>0)                                                  // теперь задаем условие если была хоть одна асимметрия то 
+    {
+        return false;                                             // возвращаем false значит не полиндром
+    }
+    else                                                          // иначе 
+    {
+        return true;                                              // полиндром асимметрии нет count = 0
+    }
 }
 
 Console.WriteLine("Введите число");                               // собственно начало программы предлагаем ввести число
 string word = Convert.ToString(Console.ReadLine());               // переводим данные в строку для удобства дальнейших иттераций (i=0 и т.д.....) в рамках метода     
-if (Razvorot (word))                                              // вызываем метод обернув в условие iF  
+if (Razvorot(word))                                              // вызываем метод обернув в условие iF  
 {
     Console.WriteLine("не полиндром");                            // если False то не полиндром
 }
@@ -18,3 +31,37 @@ else                                                              // иначе
 { 
     Console.WriteLine("Полиндром");                               // если True то полиндром
 }
+*/
+
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+Console.WriteLine("Введите координаты точки А ");                 // собираем коорлдинаты точки А
+Console.WriteLine("Введите x1(А) - ");                            // 
+int x1 = Convert.ToInt32(Console.ReadLine());                     // 
+Console.WriteLine("Введите y1(А) - ");                            // 
+int y1 = Convert.ToInt32(Console.ReadLine());                     // 
+Console.WriteLine("Введите z1(А) - ");                            // 
+int z1 = Convert.ToInt32(Console.ReadLine());                     // 
+
+
+Console.WriteLine("Введите координаты точки B ");                 // собираем координаты точки B
+Console.WriteLine("Введите x2(B) - ");                            // 
+int x2 = Convert.ToInt32(Console.ReadLine());                     // 
+Console.WriteLine("Введите y2(B) - ");                            // 
+int y2 = Convert.ToInt32(Console.ReadLine());                     // 
+Console.WriteLine("Введите z2(B) - ");                            // 
+int z2 = Convert.ToInt32(Console.ReadLine());                     // 
+
+
+Console.WriteLine($"A ({x1},{y1},{z1})");                         // показываем координаты А
+Console.WriteLine($"B ({x2},{y2},{z2})");                         // показываем координаты B
+
+double rezult = Convert.ToDouble(Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1))); // здесь основное решение с конвертацией в doudle странно сработало с первого раза без ошибок :)
+Console.WriteLine("Расстоние равно - " + rezult);                         // показываем результат  
+*/
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N ");                           // собираем коорлдинаты точки А
+int n = Convert.ToInt32(Console.ReadLine());                     // 
+
